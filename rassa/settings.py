17206bd7c4c2edb16b0db870dc182ce7286e92e6
Omar <al05-002-0323@utdelacosta.edu.mx>
2026-06-26 23:35:50 -0700

    Update settings.py

--- rassa/settings.py
+++ rassa/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "rassa.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "rassa")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
