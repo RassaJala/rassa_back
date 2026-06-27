3e3d5f2947f610aeb7f031a60a12998620a39990
Omar <al05-002-0323@utdelacosta.edu.mx>
2026-06-26 23:00:20 -0700

    Update urls.py

--- rassa/urls.py
+++ rassa/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
+from apps.accounts.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,6 +10,6 @@ urlpatterns = [
     path("api/orders/", include("apps.orders.urls")),
     path("api/categories/", include("apps.categories.urls")),
     # JWT
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
