3183cd8c8715a9074a6d732bdba2516a27c3f302
Omar <al05-002-0323@utdelacosta.edu.mx>
2026-06-26 23:35:43 -0700

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("register/", views.RegisterView.as_view(), name="register"),
     path("me/", views.MeView.as_view(), name="me"),
+    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
 ]
