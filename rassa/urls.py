d76e33b8adf0fefbd92f4fd5a0dc4caf2d2d8dba
Obed <al05-050-0322@utdelacosta.edu.mx>
2026-06-29 14:06:04 -0700

    Revert "Update URLS"
    This reverts commit 742308e3cbde380cdab4bf573b25a1dd689d909d.

--- rassa/urls.py
+++ rassa/urls.py
@@ -1,27 +1,8 @@
 from django.contrib import admin
-from django.http import JsonResponse
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
-def api_root(request):
-    return JsonResponse(
-        {
-            "message": "Rassa backend is running.",
-            "endpoints": [
-                "/admin/",
-                "/api/auth/",
-                "/api/products/",
-                "/api/orders/",
-                "/api/categories/",
-                "/api/token/",
-                "/api/token/refresh/",
-            ],
-        }
-    )
-
 urlpatterns = [
-    path("", api_root, name="api_root"),
     path("admin/", admin.site.urls),
     path("api/auth/", include("apps.accounts.urls")),
     path("api/products/", include("apps.products.urls")),
