d6bce4ba591a7b332ff48a3ca8017d4988bce751
Chava <al05-033-0323@utdelacosta.edu.mx>
2026-07-07 20:27:22 -0700

    Implementacion del middleware
    Se agregó un middleware que registra automáticamente acciones POST, PUT, PATCH y DELETE en ActivityLog.
    Se creó un endpoint de prueba en /api/test/.
    Probé con una petición real DELETE autenticada y confirmó que se creó el registro con:
    action = delete
    http_method = DELETE
    path = /api/test/
    user = middleware_tester
    Así que sí, el middleware funciona y guarda el registro cuando se ejecuta un método de escritura.

--- rassa/urls.py
+++ rassa/urls.py
@@ -7,7 +7,7 @@ Endpoints disponibles:
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from rassa.auth_serializers import CustomTokenObtainPairSerializer
