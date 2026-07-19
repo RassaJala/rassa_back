e839a601b59441168bc8a8e3cb0f7a7e4d63ad5b
luis manuel <al05-039-0323@utdelacosta.edu.mx>
2026-07-19 16:13:18 -0700

    migrate(producto-imagen): squash migrations and add drive_file_id field

---
+++ rassa/migrations/0015_productoimagen_squash_and_drive_file_id.py
@@ -0,0 +1,29 @@
+# Squash de migraciones 0013 + 0014: ProductoImagen usa solo URL (Google Drive).
+# Agrega drive_file_id para poder eliminar archivos huérfanos.
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    replaces = [
+        ("rassa", "0013_add_productoimagen_archivo"),
+        ("rassa", "0014_productoimagen_url_only"),
+    ]
+
+    dependencies = [
+        ("rassa", "0012_alter_productoimagen_options_productoimagen_orden"),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name="productoimagen",
+            name="url",
+            field=models.URLField(max_length=500),
+        ),
+        migrations.AddField(
+            model_name="productoimagen",
+            name="drive_file_id",
+            field=models.CharField(blank=True, default="", max_length=255),
+        ),
+    ]
--- rassa/models.py
+++ rassa/models.py
@@ -700,6 +700,7 @@ class ProductoImagen(models.Model):
     id_imagen = models.AutoField(primary_key=True)
     fk_producto = models.ForeignKey(Producto, on_delete=models.CASCADE, db_column="fk_producto")
     url = models.URLField(max_length=500)
+    drive_file_id = models.CharField(max_length=255, blank=True, default="")
     es_principal = models.BooleanField(default=False)
     orden = models.PositiveIntegerField(default=0)
     creado_en = models.DateTimeField(auto_now_add=True)
